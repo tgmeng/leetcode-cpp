f409810094d19d1988334bb72318c54198ef78e3
tgmeng <tgmeng@163.com>
2017-07-14 19:06:20 +0800

    init

---
+++ src/maximum-depth-of-binary-tree.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+	int maxDepth(TreeNode* root) {
+	    if(root == NULL) return 0;
+		if (root->left == NULL && root->right == NULL) return 1;
+		return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+	}
+};
---
+++ src/maximum-depth-of-binary-tree.test.h
@@ -0,0 +1,7 @@
+#include "maximum-depth-of-binary-tree.h"
+#include <gtest/gtest.h>
+
+TEST(MaximumDepthOfBinaryTree, Test) {
+  Solution sol;
+  EXPECT_EQ(1, 2);
+}
